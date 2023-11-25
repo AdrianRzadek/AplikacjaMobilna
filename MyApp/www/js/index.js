68b58a81b2eed6606694a6d6004ab23b0a31d4bd
AdrianRzadek <mschxd@gmail.com>
2023-11-25 16:08:30 +0100

    checking wich route is added/ first try implementation to deletion

--- MyApp/www/js/index.js
+++ MyApp/www/js/index.js
@@ -20,9 +20,7 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 
-
-document.addEventListener('deviceready', onDeviceReady, false);
-
+document.addEventListener("deviceready", onDeviceReady, false);
 
 // Declare a global object to store geolocation data
 //var geolocationData = {};
@@ -31,7 +29,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function getGeolocationData() {
   return new Promise((resolve, reject) => {
     // Define the success callback function
-    var onSuccess = function(position) {
+    var onSuccess = function (position) {
       geolocationData = {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
@@ -40,7 +38,7 @@ function getGeolocationData() {
         altitudeAccuracy: position.coords.altitudeAccuracy,
         heading: position.coords.heading,
         speed: position.coords.speed,
-        timestamp: position.timestamp
+        timestamp: position.timestamp,
       };
 
       // Resolve the Promise with geolocationData
@@ -58,74 +56,90 @@ function getGeolocationData() {
 }
 
 // Use the Promise to get geolocation data
-getGeolocationData()
-  .then(data => {
-    console.log(data.latitude);
-    console.log(data.longitude);
-    // You can use other geolocationData properties here
-    var x =data.latitude;
-    var y =data.longitude;
+getGeolocationData().then((data) => {
+  console.log(data.latitude);
+  console.log(data.longitude);
+  // You can use other geolocationData properties here
+  var x = data.latitude;
+  var y = data.longitude;
 
-    console.log(x);
-    console.log(y);
+  console.log(x);
+  console.log(y);
 
-var map = L.map('map').setView([x, y], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-var buttons = document.querySelectorAll('button');
-    
-    buttons.forEach(function(button) {
-        button.addEventListener("click", function() {
-          var routeId = button.value;
+  var map = L.map("map").setView([x, y], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
 
-
-          fetch(`http://localhost:3000/api/v1/points`)
-          .then(response => response.json())
-          .then(data => {
-              var routes = data.data;
-
-              // Find the selected route based on routeId
-              var selectedRoute = routes.find(route => route.hasOwnProperty(routeId));
-
-              if (selectedRoute) {
-                  var pointsArray = selectedRoute[routeId];
-
-                  console.log(`Response from points array for route ${routeId}:`, pointsArray);
-             
-       
-         
-          const waypoints = pointsArray.map(point => L.latLng(point.x, point.y));
-          waypoints.unshift(L.latLng(x, y));
-             
-L.marker([x, y]).addTo(map)
-    .bindPopup('Jesteś tutaj')
-    .openPopup();
-    L.Routing.control({
-      waypoints: waypoints,
-    }).addTo(map);
-  }
-    })
-    .catch(error => {
-      console.error('Error getting geolocation data:', error.message);
-    });
+  var buttons = document.querySelectorAll('button');
+  var routeId;
+  var routeOldId;
+  var routingControl;
+  buttons.forEach(function(button) {
+   // Store the initial value
   
-   
-  })
-})
-})
+      button.addEventListener("click", function() {
+          console.log('Button clicked. Current value:', button.value);
+          // Check if the value has changed
+         
+       
+      fetch(`http://localhost:3000/api/v1/points`)
+        .then((response) => response.json())
+        .then((data) => {
+          var routes = data.data;
+          routeId = button.value; 
+          // Find the selected route based on routeId
+          var selectedRoute = routes.find((route) =>
+            route.hasOwnProperty(routeId)
+          );
+
+          if (selectedRoute) {
+            var pointsArray = selectedRoute[routeId];
+
+            console.log(
+              `Response from points array for route ${routeId}:`,
+              pointsArray
+            );
+
+            const waypoints = pointsArray.map((point) =>
+              L.latLng(point.x, point.y)
+            );
+            waypoints.unshift(L.latLng(x, y));
+         
+
+            L.marker([x, y]).addTo(map).bindPopup("Jesteś tutaj").openPopup();
+            routingControl = L.Routing.control({
+              waypoints: waypoints,
+            }).addTo(map);
+            if(routeOldId !== routeId){
+            console.log('Value changed from', routeOldId, 'to', routeId);
+         
+           // removeAllWaypoints();
+           
+
+              L.marker([x, y]).addTo(map).bindPopup("Jesteś tutaj").openPopup();
+              routingControl = L.Routing.control({
+                waypoints: waypoints,
+              }).addTo(map);
+            }
+            
+          }
+          routeOldId = routeId; 
+        
+        })
+        .catch((error) => {
+          console.error("Error getting geolocation data:", error.message);
+        });
+      } 
+    );
+  });
+});
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+  // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-
-
-    
-
+  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+  document.getElementById("deviceready").classList.add("ready");
 }
-  
